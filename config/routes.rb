2f510b4c35bedada09384c11c7f5862fb0a63c41
wafihfzhn <hafizhuddin96@gmail.com>
2020-03-25 14:27:06 +0800

    Fix test for root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
+  root 'public/homes#index' 
+  
   devise_for :users
-
-  scope module: :public, as: :public do
-    resources :homes, only: %w[index]
-  end
 end
--- spec/routing/public/homes_routing_spec.rb
+++ spec/routing/public/homes_routing_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "Routes to the Public::HomesController", :type => :routing do
-  it "Routes a index route" do
-    expect(:get => public_homes_path).
+  it "Routes a root path" do
+    expect(:get => root_path).
       to route_to(:controller => "public/homes", :action => "index")
   end
 end
