9a892234aa9cae9b9c9d99550f853a2b6543409d
wafihfzhn <hafizhuddin96@gmail.com>
2020-03-25 09:37:21 +0800

    Testing for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,5 +23,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validation tests" do
+    it 'ensures first name presence' do
+      user = User.new(last_name: 'Last', birthday: '1996-70-27', email: 'email@sample.com', password: 'password').save
+      expect(user).to eq(false)
+    end
+
+    it 'ensures last name presence' do
+      user = User.new(first_name: 'First', birthday: '1996-07-27', email: 'email@sample.com', password: 'password').save
+      expect(user).to eq(false) 
+    end
+
+    it 'ensures email presence' do
+      user = User.new(first_name: 'First', last_name: 'last', birthday: '1996-07-27', password: 'password').save
+      expect(user).to eq(false) 
+    end
+
+    it 'ensures birthday presence' do
+      user = User.new(first_name: 'First', last_name: 'last', email: 'email@sample.com', password: 'password').save
+      expect(user).to eq(false) 
+    end
+
+    it 'ensures password presence' do
+      user = User.new(first_name: 'First', last_name: 'last', birthday: '1996-07-27', email: 'email@sample.com').save
+      expect(user).to eq(false) 
+    end
+
+    it 'should save successfully' do
+      user = User.new(first_name: 'First', last_name: 'last', birthday: '1996-07-27', email: 'email@sample.com', password: 'password').save
+      expect(user).to eq(true) 
+    end
+  end
 end
