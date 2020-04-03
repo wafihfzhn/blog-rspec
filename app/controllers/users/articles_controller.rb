8abe9403e1e038094907af20b97eecc1f153c5f8
wafihfzhn <hafizhuddin96@gmail.com>
2020-04-03 13:50:10 +0800

    implement method update article

--- app/controllers/users/articles_controller.rb
+++ app/controllers/users/articles_controller.rb
@@ -1,5 +1,6 @@
 class Users::ArticlesController < ApplicationController
   before_action :authenticate_user!
+  before_action :is_authorised, only: [:edit, :update]
 
   def index
     @articles = Article.where(user_id: current_user)
@@ -35,6 +36,11 @@ class Users::ArticlesController < ApplicationController
   end
 
   private
+
+  def is_authorised
+    @article = Article.find(params[:id])
+    redirect_to users_articles_path, alert: "You do not have permission" unless current_user.id == @article.user_id
+  end
 
   def article_params
     params.require(:article).permit(:name, :description)
